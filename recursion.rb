d8fb1bb780157ab122fb4e1100ebf1081558a900
Chris Cotton <chris.cottonn@gmail.com>
2015-03-31 21:41:42 -0700

    Added merge_sort

--- recursion.rb
+++ recursion.rb
@@ -19,9 +19,26 @@ def fibs_rec(n)
   end
 end
 
-output = []
-10.times do |i|
-  output << fibs_rec(i)
+def merge_sort(array)
+    return array if array.length == 1
+    array1 = merge_sort(array[0..array.length/2-1])
+    array2 = merge_sort(array[array.length/2..-1])
+
+    merge(array1, array2)
 end
 
-puts output.join(', ')
+def merge(array1, array2)
+  sorted = []
+
+  until array1.empty? || array2.empty?
+    if array1[0] <= array2[0]
+      sorted << array1.shift
+    else
+      sorted << array2.shift
+    end
+  end
+
+  sorted + array1 + array2
+end
+
+puts merge_sort([3, 7, 5, 5, 0, 2, 12, 40, 4]).to_s
